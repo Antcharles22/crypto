43bd79b54c7c2c9fa01a69093971e2eb63992938
Antcharles22 <121199104+Antcharles22@users.noreply.github.com>
2025-01-07 15:41:50 +0000

    first commit

---
+++ src/components/nav/nav.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+const nav = () => {
+    return (
+        <nav>
+        <ul>
+            <li>
+            <a href="/">Home</a>
+            </li>
+        </ul>
+        </nav>
+    );
+}
