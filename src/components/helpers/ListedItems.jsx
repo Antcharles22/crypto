4512626a61b56a36270880a9f4929a6962cd2b46
Antcharles22 <121199104+Antcharles22@users.noreply.github.com>
2025-03-12 11:07:53 +0000

    styling

--- src/components/helpers/ListedItems.jsx
+++ src/components/helpers/ListedItems.jsx
@@ -1,20 +1,18 @@
-import React from "react";
+// filepath: /Users/anthonycharles/cry-max/src/components/helpers/ListedItems.jsx
+import React from 'react';
 import { Link } from "react-router-dom";
 
 export default function ListedItems({ coin }) {
     return (
         <div className="home-crypto">
-        <Link to = {`/${coin.id}`}>
-            <span className="home-crypyo-image">
-            <img src={coin.image} alt={coin.name} />
-            </span>
-            <span className="home-crypto-name"> {coin.name}</span>
-
-            <span className="home-crypto-prices">
-                <span>${coin.priceBtc}BTC</span>
-                <span>${coin.priceUsd}USD</span>
-            </span>
-        </Link>
-    </div>
+            <Link to={`/${coin.id}`}>
+                <img src={coin.image} alt={coin.name} className="home-crypto-image" />
+                <span className="home-crypto-name">{coin.name}</span>
+                <div className="home-crypto-details">
+                    <span className="home-crypto-btc">{coin.priceBtc} BTC</span>
+                    <span className="home-crypto-usd">${coin.priceUsd} USD</span>
+                </div>
+            </Link>
+        </div>
     );
- }
+}
